e5374394ffa55c74389da8e27ab1ee709459514c
Harshini <hchellasamy@hawk.iit.edu>
2020-04-15 20:01:51 -0500

    fix js warning

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 function init(){
-//add your javascrip between these two lines of code
   var button = document.getElementById("entrybutton");
   var message = document.getElementById("entryinput");
   var output = document.getElementById("textoutput")
   button.addEventListener('click', function() {
-    alert("Harshini Chellasamy: "+message.value);
+    alert("Harshini Chellasamy: "+ message.value);
     output.innerHTML = message.value;
   });
 }
