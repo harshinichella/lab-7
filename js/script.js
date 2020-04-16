a97326abd5dcfecbcd5f45640686301c571db0a5
Harshini <hchellasamy@hawk.iit.edu>
2020-04-15 19:44:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
-window.addEventListener('load', init);
+  var clickButton = document.getElementById("entrybutton");
+  document.getElementById("textoutput").innerHTML = message;
+  window.addEventListener('load', init);
+}
