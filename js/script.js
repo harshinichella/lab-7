6444b94b0048681d6ba1fb268b6df61c13d5cb1f
Harshini <hchellasamy@hawk.iit.edu>
2020-04-15 19:57:15 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
-  var clickButton = document.getElementById("entrybutton");
-  document.getElementById("textoutput").innerHTML = message;
-  window.addEventListener('load', init);
+  var button = document.getElementById("entrybutton");
+  var message = document.getElementById("entryinput");
+  var output = document.getElementById("textoutput")
+  button.addEventListener('click', function() {
+    alert("Harshini Chellasamy: "+message.value);
+    output.innerHTML = message.value;
+  });
 }
+window.addEventListener('load', init);
